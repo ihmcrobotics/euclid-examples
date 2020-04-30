6b7de65858e3bd4152bde62a4eca3495348decd5
Sylvain Bertrand <sbertrand@ihmc.us>
2020-04-30 11:50:30 -0500

    Added template for creating 3D view.

---
+++ src/main/java/us/ihmc/examples/View3DFactoryExample.java
@@ -0,0 +1,45 @@
+package us.ihmc.examples;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.AmbientLight;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Box;
+import javafx.stage.Stage;
+import us.ihmc.javaFXToolkit.cameraControllers.FocusBasedCameraMouseEventHandler;
+import us.ihmc.javaFXToolkit.scenes.View3DFactory;
+
+public class View3DFactoryExample extends Application
+{
+   @Override
+   public void start(Stage primaryStage) throws Exception
+   {
+      View3DFactory view3dFactory = new View3DFactory(600, 400);
+      FocusBasedCameraMouseEventHandler cameraController = view3dFactory.addCameraController(0.001, 100.0, true);
+      cameraController.setMinLatitude(Double.NEGATIVE_INFINITY);
+      cameraController.setMaxLatitude(Double.POSITIVE_INFINITY);
+      view3dFactory.addWorldCoordinateSystem(0.1);
+      view3dFactory.addNodeToView(new AmbientLight(Color.GRAY));
+      view3dFactory.addPointLight(-10.0, 0.0, -1.0, Color.WHEAT);
+
+      // Add stuff here.
+      view3dFactory.addNodeToView(new Box(0.1, 0.1, 0.1));
+
+      primaryStage.setTitle(getClass().getSimpleName());
+      primaryStage.setMaximized(true);
+      primaryStage.setScene(view3dFactory.getScene());
+      primaryStage.setOnCloseRequest(event -> stop());
+      primaryStage.show();
+   }
+
+   @Override
+   public void stop()
+   {
+      Platform.exit();
+   }
+
+   public static void main(String[] args)
+   {
+      launch(args);
+   }
+}
