2cf2aa0c7dd2d8342e767db017c241a5bde4e87f
catee <catee@Lotor>
2020-05-14 17:00:30 -0500

    General project updates: can now model set of Euclid shapes, assign size color and position.

---
+++ src/main/java/us/ihmc/examples/ApplicationMain.java
@@ -0,0 +1,49 @@
+package us.ihmc.examples;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class ApplicationMain extends Application
+{
+	Shape3DBasics basic = new Shape3DBasics();
+	public Stage primaryStage;
+	
+   @Override
+   public void start(Stage primaryStage) throws Exception
+   {
+	   FXMLLoader loader = new FXMLLoader(getClass().getResource("Application.fxml"));
+	   BorderPane primaryPane = loader.load();
+	   Pane modelPane = new Pane();
+	   
+	   
+	  
+	   primaryPane.setCenter(modelPane);
+       modelPane.getChildren().add(Shape3DBasics.view3dFactory.getSubScene());
+      
+      
+     
+      primaryStage.setTitle(getClass().getSimpleName());
+      primaryStage.setScene(new Scene(primaryPane));
+      Shape3DBasics.view3dFactory.getSubScene().heightProperty().bind(modelPane.heightProperty());
+      Shape3DBasics.view3dFactory.getSubScene().widthProperty().bind(modelPane.widthProperty());
+      primaryStage.setOnCloseRequest(event -> stop());
+      primaryStage.show();
+      
+   }
+
+   @Override
+   public void stop()
+   {
+      Platform.exit();
+   }
+
+   public static void main(String[] args)
+   {
+      launch(args);
+   }
+}
