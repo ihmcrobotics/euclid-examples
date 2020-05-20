0ff4738ce4a4d81652b74878b9a4cf28e694dd71
catee <catee@Lotor>
2020-05-20 15:05:08 -0500

    Second Version, ready for reveiw

--- src/main/java/us/ihmc/examples/ApplicationController.java
+++ src/main/java/us/ihmc/examples/ApplicationController.java
@@ -1,376 +1,563 @@
 package us.ihmc.examples;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 
-public class ApplicationController extends Shape3DBasics {
-	
-	
-	 @FXML
-	 public Pane modelPane;
-	 
-	 @FXML
-	 public MenuButton shapeMenuButton;
-	 
-	 @FXML
-	 public MenuButton colorMenuButton;
-	 
-	 @FXML
-	 private MenuItem capsule;
+public class ApplicationController extends ShapeFactory3D
+{
 
-	 @FXML
-     private MenuItem cube;
+   @FXML
+   public Pane modelPane;
 
-     @FXML
-     private MenuItem cylinder;
+   @FXML
+   public TextField xPosition;
 
-     @FXML
-	 private MenuItem ellipsoid;
+   @FXML
+   public TextField yPosition;
 
-	 @FXML
-	 private MenuItem ramp;
+   @FXML
+   public TextField zPosition;
 
-	 @FXML
-	 private MenuItem sphere;
+   @FXML
+   private Slider heightSlider;
 
-	 @FXML
-	 private MenuItem torus;
+   @FXML
+   private Slider widthSlider;
 
-	 @FXML
-	 public  TextField xPosition;
-	 
-	 @FXML
-	 public TextField yPosition;
-	 
-	 @FXML 
-	 public TextField zPosition;
-	 
-	 @FXML
-	 private Slider heightSlider;
-	 
-	 @FXML
-	 private Slider widthSlider;
+   @FXML
+   private Slider lengthSlider;
 
-	 @FXML
-	 private Slider lengthSlider;
+   @FXML
+   private Slider radiusSlider;
 
-	 @FXML
-	 private Slider radiusSlider;
-	  
-	 @FXML
-	 public Button updateButton;
+   @FXML
+   public Button updateButton;
 
-	 @FXML
-	 public Slider yawSlider;
-	 
-	 @FXML
-	 public Slider pitchSlider;
-	 
-	 @FXML 
-	 public Slider rollSlider;
-	 
-	 @FXML
-	 public MenuItem red;
-	 
-	 @FXML
-	 public MenuItem orange;
-	 
-	 @FXML
-	 public MenuItem yellow;
-	 
-	 @FXML 
-	 public MenuItem green;
-	 
-	 @FXML
-	 public MenuItem blue;
-	 
-	 @FXML
-	 public MenuItem purple;
-	 
-	 @FXML
-	 public MenuItem black;
-	 
-	 
-	@FXML 
- 	public void makeCapsule(ActionEvent event) {
-		 resetSliders();
-		 shapeMenuButton.setText("Capsule");
-		 setPosition();
-		 setDimensions();
-		 setRadius();
-		 makeCapsule();
-	}
-	 
-	@FXML 
-	public void makeCube(ActionEvent event) {
-		resetSliders();
-		shapeMenuButton.setText("Cube");
-		setPosition();
-		setDimensions();
-		setOrientation();
-		radiusSlider.setDisable(true);
-		makeCube();
-	}
-	
-	@FXML 
-	public void makeCylinder(ActionEvent event) {
-		resetSliders();
-		shapeMenuButton.setText("Cylinder");
-		setPosition();
-		setDimensions();
-		setRadius();
-		yawSlider.setDisable(true);
-		pitchSlider.setDisable(true);
-		rollSlider.setDisable(true);
-		makeCylinder();
-	}
-	
-	@FXML 
-	public void makeEllipsoid(ActionEvent event) {
-		resetSliders();
-		shapeMenuButton.setText("Ellipsoid");
-		radiusSlider.setDisable(true);
-		setPosition();
-		setDimensions();
-		setOrientation();
-		makeEllipsoid();
-	}
-	
-	@FXML 
-	public void makeRamp(ActionEvent event) {
-		resetSliders();
-		shapeMenuButton.setText("Ramp");
-		setPosition();
-		setDimensions();
-		setOrientation();
-		radiusSlider.setDisable(true);
-		makeRamp();
-	}
-	
-	@FXML 
-	public void makeSphere(ActionEvent event) {
-		resetSliders();
-		shapeMenuButton.setText("Sphere");
-		setPosition();
-		setRadius();
-		heightSlider.setDisable(true);
-		widthSlider.setDisable(true);
-		lengthSlider.setDisable(true);
-		makeSphere();
-	}
-	
-	@FXML 
-	public void makeTorus(ActionEvent event) {
-		resetSliders();
-		shapeMenuButton.setText("Torus");
-		setRadius();
-		setPosition();
-		makeTorus();
-		yawSlider.setDisable(true);
-		pitchSlider.setDisable(true);
-		rollSlider.setDisable(true);
-	}
-	
-	@FXML
-    public void orange(ActionEvent event) {
-		color = Color.ORANGE; 
-		colorMenuButton.setText("Orange");
-    }
+   @FXML
+   public Slider yawSlider;
 
-    @FXML
-    public void purple(ActionEvent event) {
-    	color = Color.PURPLE;
-    	colorMenuButton.setText("Purple");
-    }
+   @FXML
+   public Slider pitchSlider;
 
-    @FXML
-    public void red(ActionEvent event) {
-    	color = Color.DARKRED;
-    	colorMenuButton.setText("Red");
-    }
+   @FXML
+   public Slider rollSlider;
 
-    @FXML
-    public void yellow(ActionEvent event) {
-    	color = Color.YELLOW;
-    	colorMenuButton.setText("Yellow");
-    }
+   @FXML
+   public Label heightLabel;
 
-    @FXML
-    public void green(ActionEvent event) {
-    	color = Color.DARKGREEN;
-    	colorMenuButton.setText("Green");
-    }
-    
-    @FXML 
-    public void blue(ActionEvent event) {
-    	color = Color.BLUE;
-    	colorMenuButton.setText("Blue");
-    }
-    
-    @FXML
-    public void black(ActionEvent event) {
-    	color = Color.BLACK;
-    	colorMenuButton.setText("Black");
-    }
+   @FXML
+   public Label widthLabel;
 
-	public void update(ActionEvent event) {
-		//
-	}
-    
-	
-	
-	
-	
-	
-	// INFO RETRIEVAL FUNCTIONS //
-	
-	
-	
-	
-	public void getXPosition() {
-		Double.parseDouble(xPosition.getText());
-	}
-	
-	public void getYPosition() {
-		Double.parseDouble(yPosition.getText());
-	}
-	
-	public void getZPosition() {
-		Double.parseDouble(zPosition.getText());
-	}
-	
-	public void getPosition() {
-		getXPosition();
-		getYPosition();
-		getZPosition();
-	}
-	
-	public void setXPosition() {
-		if (xPosition.getText().contentEquals("")) {
-		x = 0;
-		}
-		else {
-		x = Double.parseDouble(xPosition.getText());
-		}
-	}
-	
-	public void setYPosition() {
-		if (yPosition.getText().contentEquals("")) {
-		y = 0;
-		}
-		else {
-		y = Double.parseDouble(yPosition.getText());
-		}
-	}
-	
-	public void setZPosition() {
-		if (zPosition.getText().contentEquals("")) {
-		z = 0 ;
-		}
-		else {
-			z = Double.parseDouble(zPosition.getText());
-		}
-	}
-		
-	public void setPosition() {
-		setXPosition();
-		setYPosition();
-		setZPosition();	}
+   @FXML
+   public Label lengthLabel;
 
-	public void getYaw() {
-		yawSlider.getValue();
-	}
-	
-	public void getPitch() {
-		pitchSlider.getValue();
-	}
-	
-	public void getRoll() {
-		rollSlider.getValue();
-	}
-	
-	public void setYaw() {
-		yaw = Math.toRadians(yawSlider.getValue());
-	}
-	
-	public void setPitch() {
-		pitch = Math.toRadians(pitchSlider.getValue());
-	}
-	
-	public void setRoll() {
-		roll = Math.toRadians(rollSlider.getValue());
-	}
-	
-	public void setOrientation() {
-		setYaw();
-		setPitch();
-		setRoll();
-	}
-	
-	public void getRadius() {
-		radiusSlider.getValue();
-	}
-	
-	public void setRadius() {
-		radius = radiusSlider.getValue();
-	}
-	
-	public void getHeight() {
-		heightSlider.getValue();
-	}
-	
-	public void getLength() {
-		lengthSlider.getValue();
-	}
-	
-	public void getWidth() {
-		widthSlider.getValue();
-	}
-	
-	public void setHeight() {
-		height = heightSlider.getValue();
-	}
-	
-	public void setLength() {
-		length = lengthSlider.getValue();
-	}
-	
-	public void setWidth() {
-		width = widthSlider.getValue();
-	}
-		
-	public void setDimensions() {
-			setHeight();
-			setLength();
-			setWidth();
-	}
+   @FXML
+   public Label yawLabel;
 
-	
-	
-	
-	
-	
-	
-	public void resetSliders() {
-		radiusSlider.setDisable(false);
-		heightSlider.setDisable(false);
-		lengthSlider.setDisable(false);
-		widthSlider.setDisable(false);
-		yawSlider.setDisable(false);
-		pitchSlider.setDisable(false);
-		rollSlider.setDisable(false);
-	}
-	
-	@FXML
-	public void initialize() {
-	
-		 
-	}
+   @FXML
+   public Label pitchLabel;
+
+   @FXML
+   public Label rollLabel;
+
+   @FXML
+   public Button createSecondObjectButton;
+
+   @FXML
+   public Button clear3DSpaceButton;
+
+   @FXML
+   public ComboBox<shapes> shapeComboBox = new ComboBox<shapes>(FXCollections.observableArrayList(shapes.values()));
+
+   @FXML
+   public ComboBox<colors> colorComboBox = new ComboBox<colors>(FXCollections.observableArrayList(colors.values()));
+
+   @FXML
+   public void makeCapsule()
+   {
+      setPosition();
+      setDimensions();
+      setRadius();
+      modelCapsule();
+   }
+
+   @FXML
+   public void makeCube()
+   {
+
+      setPosition();
+      setDimensions();
+      setOrientation();
+      radiusSlider.setDisable(true);
+      modelCube();
+   }
+
+   @FXML
+   public void makeCylinder()
+   {
+      setPosition();
+      setDimensions();
+      setRadius();
+      yawSlider.setDisable(true);
+      pitchSlider.setDisable(true);
+      rollSlider.setDisable(true);
+      modelCylinder();
+   }
+
+   @FXML
+   public void makeEllipsoid()
+   {
+      ellipsoidRadiusAccomodation();
+      setPosition();
+      setDimensions();
+      setOrientation();
+      modelEllipsoid();
+   }
+
+   @FXML
+   public void makeRamp()
+   {
+
+      setPosition();
+      setDimensions();
+      setOrientation();
+      radiusSlider.setDisable(true);
+      modelRamp();
+   }
+
+   public void makeSphere()
+   {
+      setPosition();
+      setRadius();
+      heightSlider.setDisable(true);
+      widthSlider.setDisable(true);
+      lengthSlider.setDisable(true);
+      modelSphere();
+   }
+
+   @FXML
+   public void makeTorus()
+   {
+      setPosition();
+      setDimensions();
+      setOrientation();
+      torusRadiusAccomodation();
+      modelTorus();
+   }
+
+   public void makeShape()
+   {
+      shapes currentValue = shapeComboBox.getValue();
+
+      enableAllSliders();
+      orientationLabelsReset();
+
+      if (currentValue == shapes.Capsule)
+      {
+         makeCapsule();
+      }
+      if (currentValue == shapes.Cube)
+      {
+         makeCube();
+      }
+      if (currentValue == shapes.Cylinder)
+      {
+         makeCylinder();
+      }
+      if (currentValue == shapes.Ellipsoid)
+      {
+         makeEllipsoid();
+      }
+      if (currentValue == shapes.Ramp)
+      {
+         makeRamp();
+      }
+      if (currentValue == shapes.Torus)
+      {
+         makeTorus();
+      }
+      if (currentValue == shapes.Sphere)
+      {
+         makeSphere();
+      }
+   }
+
+   private void shapeListener()
+   {
+      shapeComboBox.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<shapes>()
+      {
+         @Override
+         public void changed(ObservableValue<? extends shapes> observable, shapes oldValue, shapes newValue)
+         {
+            makeShape();
+         }
+      });
+   }
+
+   private void textFieldListener()
+   {
+      xPosition.textProperty().addListener(new ChangeListener<String>()
+      {
+         @Override
+         public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue)
+         {
+            makeShape();
+         }
+      });
+      yPosition.textProperty().addListener(new ChangeListener<String>()
+      {
+         @Override
+         public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue)
+         {
+            makeShape();
+
+         }
+      });
+      zPosition.textProperty().addListener(new ChangeListener<String>()
+      {
+         @Override
+         public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue)
+         {
+            makeShape();
+
+         }
+      });
+   }
+
+   private void sliderListener()
+   {
+      radiusSlider.valueProperty().addListener(new ChangeListener<Number>()
+      {
+         @Override
+         public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue)
+         {
+            makeShape();
+         }
+      });
+      heightSlider.valueProperty().addListener(new ChangeListener<Number>()
+      {
+         @Override
+         public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue)
+         {
+            makeShape();
+         }
+      });
+      widthSlider.valueProperty().addListener(new ChangeListener<Number>()
+      {
+         @Override
+         public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue)
+         {
+            makeShape();
+         }
+      });
+      lengthSlider.valueProperty().addListener(new ChangeListener<Number>()
+      {
+         @Override
+         public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue)
+         {
+            makeShape();
+         }
+      });
+      yawSlider.valueProperty().addListener(new ChangeListener<Number>()
+      {
+         @Override
+         public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue)
+         {
+            makeShape();
+         }
+      });
+      pitchSlider.valueProperty().addListener(new ChangeListener<Number>()
+      {
+         @Override
+         public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue)
+         {
+            makeShape();
+         }
+      });
+      rollSlider.valueProperty().addListener(new ChangeListener<Number>()
+      {
+         @Override
+         public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue)
+         {
+            makeShape();
+         }
+      });
+   }
+
+   private void colorListener()
+   {
+      colorComboBox.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<colors>()
+      {
+         @Override
+         public void changed(ObservableValue<? extends colors> observable, colors oldValue, colors newValue)
+         {
+            changeColor();
+            makeShape();
+         }
+      });
+   }
+
+   public void changeColor()
+   {
+      colors currentValue = colorComboBox.getValue();
+      if (currentValue == colors.Red)
+      {
+         color = Color.RED;
+      }
+      if (currentValue == colors.Orange)
+      {
+         color = Color.ORANGE;
+      }
+      if (currentValue == colors.Yellow)
+      {
+         color = Color.YELLOW;
+      }
+      if (currentValue == colors.Green)
+      {
+         color = Color.GREEN;
+      }
+      if (currentValue == colors.Blue)
+      {
+         color = Color.BLUE;
+      }
+      if (currentValue == colors.Purple)
+      {
+         color = Color.PURPLE;
+      }
+      if (currentValue == colors.Black)
+      {
+         color = Color.BLACK;
+      }
+   }
+
+   // INFO RETRIEVAL FUNCTIONS //
+   public void setXPosition()
+   {
+      if (xPosition.getText().contentEquals(""))
+      {
+         x = 0;
+      }
+      else
+      {
+         x = Double.parseDouble(xPosition.getText());
+      }
+   }
+
+   public void setYPosition()
+   {
+      if (yPosition.getText().contentEquals(""))
+      {
+         y = 0;
+      }
+      else
+      {
+         y = Double.parseDouble(yPosition.getText());
+      }
+   }
+
+   public void setZPosition()
+   {
+      if (zPosition.getText().contentEquals(""))
+      {
+         z = 0;
+      }
+      else
+      {
+         z = Double.parseDouble(zPosition.getText());
+      }
+   }
+
+   public void setPosition()
+   {
+      setXPosition();
+      setYPosition();
+      setZPosition();
+   }
+
+   public void setYaw()
+   {
+      yaw = Math.toRadians(yawSlider.getValue());
+   }
+
+   public void setPitch()
+   {
+      pitch = Math.toRadians(pitchSlider.getValue());
+   }
+
+   public void setRoll()
+   {
+      roll = Math.toRadians(rollSlider.getValue());
+   }
+
+   public void setOrientation()
+   {
+      setYaw();
+      setPitch();
+      setRoll();
+   }
+
+   public void setRadius()
+   {
+      radius = radiusSlider.getValue();
+   }
+
+   public void setHeight()
+   {
+      height = heightSlider.getValue();
+   }
+
+   public void setLength()
+   {
+      length = lengthSlider.getValue();
+   }
+
+   public void setWidth()
+   {
+      width = widthSlider.getValue();
+   }
+
+   public void setDimensions()
+   {
+      setHeight();
+      setLength();
+      setWidth();
+   }
+
+   public void enableAllSliders()
+   {
+      radiusSlider.setDisable(false);
+      heightSlider.setDisable(false);
+      lengthSlider.setDisable(false);
+      widthSlider.setDisable(false);
+      yawSlider.setDisable(false);
+      pitchSlider.setDisable(false);
+      rollSlider.setDisable(false);
+   }
+
+   @FXML
+   public void createSecondObject(ActionEvent event)
+   {
+      multipleNodes = true;
+      saveCurrentNode();
+      multipleNodeViewing();
+      makeShape();
+   }
+
+   public void saveCurrentNode()
+   {
+      shapes currentValue = shapeComboBox.getValue();
+
+      if (currentValue == shapes.Capsule)
+      {
+         saveNode = javaFXCapsuleNode;
+      }
+      if (currentValue == shapes.Cube)
+      {
+         saveNode = javaFXCubeNode;
+      }
+      if (currentValue == shapes.Cylinder)
+      {
+         saveNode = javaFXCylinderNode;
+      }
+      if (currentValue == shapes.Ellipsoid)
+      {
+         saveNode = javaFXEllipsoidNode;
+      }
+      if (currentValue == shapes.Ramp)
+      {
+         saveNode = javaFXRampNode;
+      }
+      if (currentValue == shapes.Torus)
+      {
+         saveNode = javaFXTorusNode;
+      }
+      if (currentValue == shapes.Sphere)
+      {
+         saveNode = javaFXSphereNode;
+      }
+   }
+
+   public void clear3DSpace(ActionEvent event)
+   {
+      multipleNodes = false;
+      makeNewView3DFactory();
+      resetSliderValues();
+   }
+
+   public void resetSliderValues() {
+      heightSlider.adjustValue(0.2);
+      widthSlider.adjustValue(0.2);
+      lengthSlider.adjustValue(0.2);
+      radiusSlider.adjustValue(0.2);
+      pitchSlider.adjustValue(0);
+      rollSlider.adjustValue(0);
+      yawSlider.adjustValue(0);
+   }
+   
+   public enum shapes
+   {
+      Capsule, Cube, Cylinder, Ellipsoid, Ramp, Sphere, Torus
+   }
+
+   public enum colors
+   {
+      Red, Orange, Yellow, Green, Blue, Purple, Black
+   }
+
+   public void setComboBoxOptions()
+   {
+      shapeComboBox.getItems().addAll(FXCollections.observableArrayList(shapes.values()));
+      colorComboBox.getItems().addAll(FXCollections.observableArrayList(colors.values()));
+   }
+
+   public void ellipsoidRadiusAccomodation()
+   {
+      radiusSlider.setDisable(true);
+      heightLabel.setText("x Radius");
+      widthLabel.setText("y Radius");
+      lengthLabel.setText("z Radius");
+
+   }
+
+   public void torusRadiusAccomodation()
+   {
+      heightLabel.setText("xy Radius");
+      widthLabel.setText("z Radius");
+      lengthSlider.setDisable(true);
+      radiusSlider.setDisable(true);
+      yawSlider.setDisable(true);
+      pitchSlider.setDisable(true);
+      rollSlider.setDisable(true);
+   }
+
+   public void orientationLabelsReset()
+   {
+      heightLabel.setText("Height");
+      widthLabel.setText("Width");
+      lengthLabel.setText("Length");
+      pitchLabel.setText("Pitch");
+      yawLabel.setText("Yaw");
+      rollLabel.setText("Roll");
+
+   }
+
+   @FXML
+   public void initialize()
+   {
+      setComboBoxOptions();
+      shapeListener();
+      textFieldListener();
+      sliderListener();
+      colorListener();
+   }
 }
--- src/main/java/us/ihmc/examples/ApplicationMain.java
+++ src/main/java/us/ihmc/examples/ApplicationMain.java
@@ -9,25 +9,25 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class ApplicationMain extends Application
-{	
-	
+{
+
    @Override
    public void start(Stage primaryStage) throws Exception
    {
-	   FXMLLoader loader = new FXMLLoader(getClass().getResource("Application.fxml"));
-	   BorderPane primaryPane = loader.load();
-	   Pane modelPane = new Pane();
-	   
-	   primaryPane.setCenter(modelPane);
-       modelPane.getChildren().add(Shape3DBasics.view3dFactory.getSubScene());
-      
+      FXMLLoader loader = new FXMLLoader(getClass().getResource("Application.fxml"));
+      BorderPane primaryPane = loader.load();
+      Pane modelPane = new Pane();
+
+      primaryPane.setCenter(modelPane);
+      modelPane.getChildren().add(ShapeFactory3D.view3dFactory.getSubScene());
+
       primaryStage.setTitle(getClass().getSimpleName());
       primaryStage.setScene(new Scene(primaryPane));
-      Shape3DBasics.view3dFactory.getSubScene().heightProperty().bind(modelPane.heightProperty());
-      Shape3DBasics.view3dFactory.getSubScene().widthProperty().bind(modelPane.widthProperty());
+      ShapeFactory3D.view3dFactory.getSubScene().heightProperty().bind(modelPane.heightProperty());
+      ShapeFactory3D.view3dFactory.getSubScene().widthProperty().bind(modelPane.widthProperty());
       primaryStage.setOnCloseRequest(event -> stop());
       primaryStage.show();
-      
+
    }
 
    @Override
