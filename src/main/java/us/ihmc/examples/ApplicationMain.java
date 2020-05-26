edc92614e8640d9ffd3a3152dbec0a58e43256d1
catee <catee@Lotor>
2020-05-26 11:13:02 -0500

    Merged ShapeFactory3D to ApplicationController

--- src/main/java/us/ihmc/examples/ApplicationMain.java
+++ src/main/java/us/ihmc/examples/ApplicationMain.java
@@ -4,12 +4,17 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.SceneAntialiasing;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import us.ihmc.javaFXToolkit.scenes.View3DFactory;
+import us.ihmc.javaFXToolkit.scenes.View3DFactory.SceneType;
 
 public class ApplicationMain extends Application
 {
+
+   public static View3DFactory view3dFactory = new View3DFactory(298, 300, true, SceneAntialiasing.BALANCED, SceneType.SUB_SCENE);
 
    @Override
    public void start(Stage primaryStage) throws Exception
@@ -19,12 +24,12 @@ public class ApplicationMain extends Application
       Pane modelPane = new Pane();
 
       primaryPane.setCenter(modelPane);
-      modelPane.getChildren().add(ShapeFactory3D.view3dFactory.getSubScene());
+      modelPane.getChildren().add(ApplicationMain.view3dFactory.getSubScene());
 
       primaryStage.setTitle(getClass().getSimpleName());
       primaryStage.setScene(new Scene(primaryPane));
-      ShapeFactory3D.view3dFactory.getSubScene().heightProperty().bind(modelPane.heightProperty());
-      ShapeFactory3D.view3dFactory.getSubScene().widthProperty().bind(modelPane.widthProperty());
+      ApplicationMain.view3dFactory.getSubScene().heightProperty().bind(modelPane.heightProperty());
+      ApplicationMain.view3dFactory.getSubScene().widthProperty().bind(modelPane.widthProperty());
       primaryStage.setOnCloseRequest(event -> stop());
       primaryStage.show();
 
